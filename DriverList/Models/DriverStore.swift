6993b6eb4236d3e82f1c53fcd434c70cf01b80c7
Guillaume MAIANO <guillaume@maiano.fr>
2020-12-21 18:07:56 +0100

    Creates the network request

--- DriverList/Models/DriverStore.swift
+++ DriverList/Models/DriverStore.swift
@@ -14,12 +14,26 @@ class DriverStore {
     
     func refreshData() -> Void {
         // request a refresh via NSURLConnection
-        let url = URL(string: "https://service.drivequant.com/v2")! // should be better encapsulated
+        let urlString = "https://service.drivequant.com/v2"
+        var urlBuilder = URLComponents(string: urlString)
+        urlBuilder?.queryItems = [
+            // rankingDepth=5&rankingPeriod=ALL_TIME&rankingType=safety&groupName=All
+            URLQueryItem(name: "rankingDepth", value: "5"),
+            URLQueryItem(name: "rankingPeriod", value: "ALL_TIME"), //Parameters should have their own struct:String
+            URLQueryItem(name: "rankingType", value: "safety"), // same remark
+            URLQueryItem(name: "groupName", value: "All")
+        ]
+        guard let url = urlBuilder?.url else {
+            print("URL could not be constructed \(String(describing: urlBuilder?.debugDescription))")
+            return
+        }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        request.addValue("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqZXJlbXkuYmF5bGVAZHJpdmVxdWFudC5jb20iLCJoeXBlckFkbWluIjpmYWxzZSwiYXVkaWVuY2UiOiJtb2JpbGUiLCJjcmVhdGVkIjoxNjA0Mzk3MjQ5OTc4LCJleHAiOjE2MTIxNzMyNDksImN1c3RvbWVyIjoiVzRuRHZOc3Q5cjdDZDF4bUlCMWVpWmlFIn0.dqQgljBQQJ3S4KzHqa51_ICbe2f7Z_gfuOOQt9piPX9_vos6nEhMryXB6puvRgAcEChAyMjYh6OrP-ZARpMkoQ", forHTTPHeaderField: "Authorization")
-        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.main, completionHandler: {_,_,_ in print("NOT IMPLEMENTED")})
-        // convert to datatask with request lol...
+        request.setValue("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqZXJlbXkuYmF5bGVAZHJpdmVxdWFudC5jb20iLCJoeXBlckFkbWluIjpmYWxzZSwiYXVkaWVuY2UiOiJtb2JpbGUiLCJjcmVhdGVkIjoxNjA0Mzk3MjQ5OTc4LCJleHAiOjE2MTIxNzMyNDksImN1c3RvbWVyIjoiVzRuRHZOc3Q5cjdDZDF4bUlCMWVpWmlFIn0.dqQgljBQQJ3S4KzHqa51_ICbe2f7Z_gfuOOQt9piPX9_vos6nEhMryXB6puvRgAcEChAyMjYh6OrP-ZARpMkoQ", forHTTPHeaderField: "Authorization")
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // get the data and store it in the drivers array
+            print(response.debugDescription)
+        }.resume()
         
     }
 }
