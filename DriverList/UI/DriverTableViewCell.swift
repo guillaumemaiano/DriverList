8aa8e7525d6657699b63c3f6b7a14b647cab14ac
Guillaume MAIANO <guillaume@maiano.fr>
2020-12-21 16:13:58 +0100

    Basic cell with required data

--- DriverList/UI/DriverTableViewCell.swift
+++ DriverList/UI/DriverTableViewCell.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 class DriverTableViewCell: UITableViewCell {
-    
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    // Kind of a data protection leak, since it's actually emails...
+    @IBOutlet weak var usrIdLabel: UILabel!
 }
