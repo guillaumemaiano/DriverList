cf84c9b867d86d51928bca57aa3338f4dfb89e55
Guillaume MAIANO <guillaume@maiano.fr>
2020-12-22 22:02:23 +0100

    Implements a quick 'Observable'-based store

--- DriverList/VM/DriverStoreViewModel.swift
+++ DriverList/VM/DriverStoreViewModel.swift
@@ -7,10 +7,35 @@
 
 import Foundation
 
-class DriverStoreViewModel {
-    var driverStore = DriverStore()
+class DriverStoreViewModel: ViewModel {
+    var items: DLObservable<[DriverInformation]>
+    
+    private var driverStore = DriverStore()
+    private var executeAction: (([DriverInformation]) -> Void)?;
+    
+    func bindAction(action: @escaping (([DriverInformation]) -> Void)) {
+        executeAction = action
+    }
+    
+    init() {
+        items = driverStore.drivers
+        driverStore.drivers.observe({
+            driversInfoArray in
+            self.executeAction?(driversInfoArray)
+        })
+    }
     
     func requestRefresh() {
         driverStore.refreshData()
     }
 }
+
+protocol ViewModelActions {
+    func requestRefresh()
+}
+
+protocol ViewModelDataAccess {
+    var items: DLObservable<[DriverInformation]> { get }
+}
+
+protocol ViewModel: ViewModelActions, ViewModelDataAccess {}
